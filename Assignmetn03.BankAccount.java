8feec1eb15faf2af93c892fcbd1e1030aaee96f1
Omar0218 <72273360+Omar0218@users.noreply.github.com>
2021-04-24 01:48:11 -0500

    Add files via upload

---
+++ Assignmetn03.BankAccount.java
@@ -0,0 +1,37 @@
+
+package assignment03;
+
+class BankAccount {
+    
+    private String owner;
+    private double balance;
+    public String getOwner(){
+        return this.owner;
+    }
+    
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
+    
+    public double getBalance(){
+        return balance;
+    }
+    
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    
+    public BankAccount(String owner, double balance){
+        this.owner = owner;
+        this.balance = balance;
+    }
+    
+    public void deposit(double amount){
+        balance += amount;
+    }
+    
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+    
+}
